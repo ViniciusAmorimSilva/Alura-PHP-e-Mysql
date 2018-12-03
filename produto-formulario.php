21d3f707c1aebafe0f0e8c3af922a89f353b79df
ViniciusAmorimSilva <amorimm.vinicius@gmail.com>
2018-12-02 23:16:40 -0200

    Criando Funções em PHP

--- produto-formulario.php
+++ produto-formulario.php
@@ -1,11 +1,20 @@
 <?php include("cabecalho.php"); ?>
 	<h1>Formulário de produto</h1>
 	<form action="adiciona-produto.php">
-		Nome:
-		<input type="text" name="nome"><br/>
-		Preço:
-		<input type="number" name="preco"></br>
-
-		<input type="submit" name="cadastrar">
+		<table class="table">
+			<tr>
+				<td>Nome</td>
+				<td><input class="form-control" type="text" name="nome"><br/></td>
+			</tr>
+			<tr>
+				<td>Preço</td>
+				<td><input class="form-control"type="number" name="preco"></br></td>
+			</tr>
+			<tr>
+				<td>
+					<button class="btn btn-primary" type="submit">Cadastrar</button>
+				</td>
+			</tr>
+		</table>
 	</form>
 <?php include("rodape.php"); ?>
