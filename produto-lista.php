64e19f951d4f5bda430cf2402a28a8380ba65e90
ViniciusAmorimSilva <amorimm.vinicius@gmail.com>
2018-12-05 22:37:03 -0200

    Melhorando listagem de produtos - alternativa ao endforeach

--- produto-lista.php
+++ produto-lista.php
@@ -3,24 +3,18 @@
 <?php include("banco-produto.php"); ?>
 
 
-
-<?php
-
-$produtos = listaProdutos($conexao);
-#foreach ($produtos as $produto) {
-#	echo $produto['nome'] . "<br/>";
-#}
-?>
 <table class="table table-striped table-bordered">
-<?php
-foreach ($produtos as $produto) {
-?>
-		<tr>	
-			<td><?= $produto['nome'] ?></td>
-			<td><?= $produto['preco'] ?></td>
-		</tr>
-<?php
-}
-?>	
-</table>	
+	<?php
+		$produtos = listaProdutos($conexao);		
+		foreach ($produtos as $produto) {
+	?>
+	<tr>	
+		<td><?= $produto['nome'] ?></td>
+		<td><?= $produto['preco'] ?></td>
+	</tr>
+	<?php
+		endforeach
+	?>	
+</table>
+
 <?php include("rodape.php"); ?>
