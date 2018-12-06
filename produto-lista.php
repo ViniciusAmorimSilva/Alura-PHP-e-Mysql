5cb2c76a8dc0ac01412764de3e68dd2f06a1b8d1
ViniciusAmorimSilva <amorimm.vinicius@gmail.com>
2018-12-05 22:33:23 -0200

    Melhorando a listagem de produtos

--- produto-lista.php
+++ produto-lista.php
@@ -1,11 +1,26 @@
 <?php include("cabecalho.php"); ?>
-<?php include("conecta.php");
+<?php include("conecta.php"); ?>
+<?php include("banco-produto.php"); ?>
 
-$resultado = mysqli_query($conexao, "select * from produtos");
-while($produto = mysqli_fetch_assoc($resultado)) {
-	echo $produto["nome"] . "</br>";
+
+
+<?php
+
+$produtos = listaProdutos($conexao);
+#foreach ($produtos as $produto) {
+#	echo $produto['nome'] . "<br/>";
+#}
+?>
+<table class="table table-striped table-bordered">
+<?php
+foreach ($produtos as $produto) {
+?>
+		<tr>	
+			<td><?= $produto['nome'] ?></td>
+			<td><?= $produto['preco'] ?></td>
+		</tr>
+<?php
 }
-
-
 ?>	
+</table>	
 <?php include("rodape.php"); ?>
